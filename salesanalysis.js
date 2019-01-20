52850eee0071060b3e66c5eaa587aaca36de2eba
Ruby Barry <rubypbarry@gmail.com>
2019-01-20 15:54:41 -0500

    completed project

--- salesanalysis.js
+++ salesanalysis.js
@@ -1,3 +1,104 @@
-const productsPurchased = () => {
-    
+const productsPurchased = (orderArr, prodArr) => {
+    return prodArr.filter((product) => {
+        return orderArr.find((order) => {
+            return product.id === order.productId;
+        })
+    })
 }
+
+const topSellingProductByQuantity = (orderArr, prodArr) => {
+    let productsByQuantitySold = orderArr.reduce((accum, order) => {
+        let currentProduct = prodArr.find((elem) => {
+            return elem.id === order.productId;
+        });
+            if (accum.find(elem => elem.name === currentProduct.name)){
+                let idx = accum.findIndex(elem => elem.name === currentProduct.name);
+                accum[idx].quantity += order.quantity;
+            }
+        else {
+            currentProduct.quantity = order.quantity
+            accum.push(currentProduct);
+        }
+        return accum;
+    }, [] );
+
+   productsByQuantitySold.sort(function(a, b){
+        if (b.quantity > a.quantity){
+            return 1;
+        } else if (a.quantity > b.quantity){
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    return productsByQuantitySold[0]; 
+}
+
+const usersWithOrders = (userArr, orderArr) => {
+    return userArr.reduce((accum, user) => {
+        if (orderArr.find(order => order.userId === user.id)){
+            accum.push(user);
+        }
+        return accum;
+    }, []);
+}
+
+const products = [
+    {
+      id: 1,
+      name: 'foo',
+      price: 7
+    },
+    {
+      id: 2,
+      name: 'bar',
+      price: 2
+    },
+    {
+      id: 5,
+      name: 'bazz',
+      price: 1
+    },
+  ];
+  
+  const users = [
+    {
+       id: 1,
+       name: 'moe'
+    },
+    {
+       id: 2,
+       name: 'larry'
+    },
+    {
+       id: 3,
+       name: 'curly'
+    }
+  ];
+  
+  const orders = [
+    {
+      id: 1,
+      productId: 1,
+      quantity: 3,
+      userId: 1
+    },
+    {
+      id: 2,
+      productId: 1,
+      quantity: 7,
+      userId: 1
+    },
+    {
+      id: 3,
+      productId: 5,
+      quantity: 70,
+      userId: 3
+    },
+    {
+      id: 3,
+      productId: 5,
+      quantity: 1,
+      userId: 3
+    }
+  ];
--- salesanalysis.spec.js
+++ salesanalysis.spec.js
@@ -1,68 +1,47 @@
-beforeEach(() => {
-    const products = [
-        {
-          id: 1,
-          name: 'foo',
-          price: 7
-        },
-        {
-          id: 2,
-          name: 'bar',
-          price: 2
-        },
-        {
-          id: 5,
-          name: 'bazz',
-          price: 1
-        },
-      ];
-      
-      const users = [
-        {
-           id: 1,
-           name: 'moe'
-        },
-        {
-           id: 2,
-           name: 'larry'
-        },
-        {
-           id: 3,
-           name: 'curly'
-        }
-      ];
-      
-      // productId matches up with product in products
-      // userId matches up with user in users
-      const orders = [
-        {
-          id: 1,
-          productId: 1,
-          quantity: 3,
-          userId: 1
-        },
-        {
-          id: 2,
-          productId: 1,
-          quantity: 7,
-          userId: 1
-        },
-        {
-          id: 3,
-          productId: 5,
-          quantity: 70,
-          userId: 3
-        },
-        {
-          id: 3,
-          productId: 5,
-          quantity: 1,
-          userId: 3
-        }
-      ];
-})
 describe('products purchased', function(){
-   it('is a function', function (){
-       expect( typeof productsPurchased ).toEqual('function');
-   })
-})
+    it('is a function', function(){
+        expect(typeof productsPurchased).toEqual('function');
+    });
+    it('returns an array of objects purchased', function(){
+        expect( productsPurchased(orders, products) ).toEqual( [ {
+            id: 1,
+            name: 'foo',
+            price: 7
+          }, 
+          {
+            id: 5,
+            name: 'bazz',
+            price: 1
+          }] );
+    });
+});
+
+describe('top selling product by quantity', function(){
+    it('is a function', function(){
+        expect(typeof topSellingProductByQuantity).toEqual('function');
+    });
+    it('returns the top selling product object', function(){
+        expect( topSellingProductByQuantity(orders, products) ).toEqual(  {
+            id: 5,
+            name: 'bazz',
+            price: 1,
+            quantity: 71,
+          } );
+    });
+});
+
+describe('users with orders', function(){
+    it('is a function', function(){
+        expect(typeof usersWithOrders).toEqual('function');
+    });
+    it('returns only users who have placed an order', function(){
+        expect( usersWithOrders(users, orders)).toEqual([ {
+            id: 1,
+            name: 'moe'
+         },  
+         {
+            id: 3,
+            name: 'curly'
+         }]);
+    });
+});
